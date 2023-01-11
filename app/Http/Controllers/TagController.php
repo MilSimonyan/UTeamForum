5f43e30ea4ea4e721c9189b40b0d214131a29dc9
MilSimonyan <millsimonyan01@gmail.com>
2023-01-11 19:07:14 +0400

    add tag CRUD

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tag;
+use App\Repositories\TagRepository;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class TagController extends Controller
+{
+    protected TagRepository $tagRepository;
+
+    public function __construct(TagRepository $tagRepository){
+        $this->tagRepository = $tagRepository;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index() : JsonResponse
+    {
+        return new JsonResponse($this->tagRepository->findAll(), JsonResponse::HTTP_OK);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request) : JsonResponse
+    {
+        $this->validate($request, [
+           'name' => ['required', 'string','min:2','max:30', 'unique:tags,name']
+        ]);
+
+        $tag = new Tag();
+        $tag->name = $request->get('name');
+        $tag->save();
+
+        return new JsonResponse($tag, JsonResponse::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(int $id) : JsonResponse
+    {
+        return new JsonResponse($this->tagRepository->find($id), JsonResponse::HTTP_OK);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, int $id) : JsonResponse
+    {
+        $this->validate($request, [
+            'name' => ['string','min:2','max:30', 'unique:tags,name']
+        ]);
+
+        $tag = $this->tagRepository->find($id);
+        $tag->name = $request->get('name', $tag->name);
+        $tag->save();
+
+        return new JsonResponse($tag, JsonResponse::HTTP_OK);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy(int $id) : JsonResponse
+    {
+        $tag = $this->tagRepository->find($id,);
+        $tag->delete();
+
+        return new JsonResponse(null,  JsonResponse::HTTP_NO_CONTENT);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TagController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::controller(TagController::class)
+    ->prefix('/tag')
+    ->group(function () {
+        Route::get('/get', 'index');
+        Route::get('/get/{id}', 'show');
+        Route::post('/create', 'store');
+        Route::patch('/edit/{id}', 'update');
+        Route::delete('/delete/{id}', 'destroy');
+    });
